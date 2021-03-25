4037e3e829f7969d245da96ab292ccf46241d7b2
Muhammad Salif <muhammad.salif@yahoo.com>
2021-03-26 00:11:20 +0500

    Minor

---
+++ tempratureSensor/tempratureSensor.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+
+void loop()
+{
+
+  Serial.print("Internal Temprature Sensor");
+  Serial.print("Internal Temprature Sensor");
+  Serial.println(" C");
+  delay(2000);
+
+}
